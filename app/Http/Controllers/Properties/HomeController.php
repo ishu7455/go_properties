6d518944ba2cc1930966b19017f1e2984416b780
ishu7455 <ishudhariya716@gmail.com>
2023-06-01 00:21:06 +0530

    HomeController

---
+++ app/Http/Controllers/Properties/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Properties;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+   public function home(){
+      return "hello";
+   }
+
+   public function ishu(){
+      echo "hjjkk";
+   }
+
+}
